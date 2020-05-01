09d39cb91ecd3684956a3076be7dfa5ca450b20b
Roman93 <bat1238@yandex.ru>
2020-05-01 19:20:37 +0700

    Added some tests fo validation

--- src/test/java/com/notification/controller/EmailEndpointTest.java
+++ src/test/java/com/notification/controller/EmailEndpointTest.java
@@ -5,6 +5,7 @@ import com.notification.config.WebServiceTestConfiguration;
 import com.notification.dto.EmailRequest;
 import com.notification.dto.EmailResponse;
 import com.notification.service.EmailService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.ws.test.server.MockWebServiceClient;
 import org.springframework.xml.transform.StringResult;
@@ -26,6 +28,7 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.ws.test.server.RequestCreators.withPayload;
+import static org.springframework.ws.test.server.ResponseMatchers.clientOrSenderFault;
 import static org.springframework.ws.test.server.ResponseMatchers.noFault;
 import static org.springframework.ws.test.server.ResponseMatchers.payload;
 import static org.springframework.ws.test.server.ResponseMatchers.validPayload;
@@ -36,8 +39,11 @@ import static org.springframework.ws.test.server.ResponseMatchers.validPayload;
  * @author Roman Batygin
  */
 @RunWith(SpringRunner.class)
+@TestPropertySource("classpath:application.properties")
 @Import({WebServiceTestConfiguration.class, EmailEndpoint.class})
 public class EmailEndpointTest {
+
+    private static final int LARGE_SUBJECT_LENGTH = 256;
 
     @Inject
     private ApplicationContext applicationContext;
@@ -66,6 +72,30 @@ public class EmailEndpointTest {
                 .andExpect(noFault())
                 .andExpect(payload(response))
                 .andExpect(validPayload(xsdSchema));
+    }
+
+    @Test
+    public void testInvalidEmailSaving() throws IOException {
+        EmailRequest emailRequest = TestHelperUtils.createEmailRequest();
+        emailRequest.setReceiver("email");
+        StringSource request = getPayload(emailRequest);
+        mockClient.sendRequest(withPayload(request)).andExpect(clientOrSenderFault());
+    }
+
+    @Test
+    public void testEmptyMessageSaving() throws IOException {
+        EmailRequest emailRequest = TestHelperUtils.createEmailRequest();
+        emailRequest.setMessage(StringUtils.EMPTY);
+        StringSource request = getPayload(emailRequest);
+        mockClient.sendRequest(withPayload(request)).andExpect(clientOrSenderFault());
+    }
+
+    @Test
+    public void testLargeSubjectSaving() throws IOException {
+        EmailRequest emailRequest = TestHelperUtils.createEmailRequest();
+        emailRequest.setSubject(StringUtils.repeat('Q', LARGE_SUBJECT_LENGTH));
+        StringSource request = getPayload(emailRequest);
+        mockClient.sendRequest(withPayload(request)).andExpect(clientOrSenderFault());
     }
 
     private StringSource getPayload(Object object) {
